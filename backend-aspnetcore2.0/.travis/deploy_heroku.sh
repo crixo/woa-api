4993e1645e1b7bf1dad866c95ef3cead57ee2b82
cristiano-degiorgis <cri@webprofessor.it>
2018-01-07 15:12:39 +0100

    travis docker 3

--- backend-aspnetcore2.0/.travis/deploy_heroku.sh
+++ backend-aspnetcore2.0/.travis/deploy_heroku.sh
@@ -1,13 +1,13 @@
 # https://docs.travis-ci.com/user/deployment/heroku/
 #!/bin/sh
-# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-# heroku plugins:install heroku-container-registry
-# docker login -e _ -u _ --password=$HEROKU_AUTHTOKEN registry.heroku.com
-# heroku container:push web --app $HEROKU_APP_NAME
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-container-registry
+docker login -e _ -u _ --password=$HEROKU_AUTHTOKEN registry.heroku.com
+heroku container:push web --app woa-api-sample
 
 #- heroku plugins:install heroku-container-registry
 #- export TAG=$REPO-heroku:$COMMIT
-docker build -f heroku.Dockerfile -t $TAG .
-docker tag $TAG registry.heroku.com/woa-api-sample/web
-docker login --username=$HEROKUU_USERNAME --password=$HEROKU_AUTHTOKEN registry.heroku.com
-docker push registry.heroku.com/woa-api-sample/web
+#docker build -f heroku.Dockerfile -t $TAG .
+#docker tag $TAG registry.heroku.com/woa-api-sample/web
+#docker login -e _ -u _ --password=$HEROKU_AUTHTOKEN registry.heroku.com
+#docker push registry.heroku.com/woa-api-sample/web
