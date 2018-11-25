dce1bd80a951a1b7c49faa21a7a7dd576708e44f
cristiano-degiorgis <cri@webprofessor.it>
2018-11-25 14:03:23 +0100

    travis

--- backend-aspnetcore2.0/heroku-container-release.sh
+++ backend-aspnetcore2.0/heroku-container-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker push registry.heroku.com/$REPO/web;
-imageId=$(docker inspect registry.heroku.com/$REPO/web --format={{.Id}})
+docker push registry.heroku.com/$HEROKU_APP/web;
+imageId=$(docker inspect registry.heroku.com/$HEROKU_APP/web --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
-curl -n -X PATCH https://api.heroku.com/apps/$REPO/formation \
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
 -d "$payload" \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
