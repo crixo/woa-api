e98840d6f9ab984e511883bcd68db7d270db5155
cristiano-degiorgis <cri@webprofessor.it>
2018-11-25 15:06:03 +0100

    travis

--- heroku-container-release.sh
+++ heroku-container-release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-docker push registry.heroku.com/$HEROKU_APP/web;
 imageId=$(docker inspect registry.heroku.com/$HEROKU_APP/web --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
 curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
