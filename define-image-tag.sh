7affec0d31af9f4836a5c0526f1012e326984c23
cristiano-degiorgis <cri@webprofessor.it>
2018-11-25 16:30:13 +0100

    travis tagging

--- define-image-tag.sh
+++ define-image-tag.sh
@@ -4,5 +4,5 @@
 #cd backend-aspnetcore2.0
 #cd .. && echo $PWD
 [  -z "$TRAVIS_TAG" ] && IMAGE_TAG_TMP=latest || IMAGE_TAG_TMP=$TRAVIS_TAG
-#IMAGE_TAG=$IMAGE_TAG_TMP
+export IMAGE_TAG=$IMAGE_TAG_TMP
 echo $IMAGE_TAG_TMP
